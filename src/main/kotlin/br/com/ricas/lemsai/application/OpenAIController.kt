458ee96ca04f71dfc5cf9c7bf02f2e13a5e988ea
Ricardo <ricardohsmello@gmail.com>
2023-09-03 02:17:08 -0300

    adjust

---
+++ src/main/kotlin/br/com/ricas/lemsai/application/OpenAIController.kt
@@ -0,0 +1,17 @@
+package br.com.ricas.lemsai.application
+
+import br.com.ricas.lemsai.domain.OpenAIService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class OpenAIController(
+    val openAIService: OpenAIService
+) {
+    @PostMapping("/ai")
+    fun call(@RequestParam question: String) {
+        val exec = openAIService.exec("quem descobriu o brasil")
+
+        println(exec)
+
+    }
+}
