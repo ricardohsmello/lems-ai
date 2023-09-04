7de1c1da4eaccde74bddae7ffd44df6630413e68
Ricardo <ricardohsmello@gmail.com>
2023-09-03 21:33:12 -0300

    adding resttemplate bean class

---
+++ src/main/kotlin/br/com/ricas/lemsai/domain/OpenAIServiceImpl.kt
@@ -0,0 +1,20 @@
+package br.com.ricas.lemsai.domain
+
+import br.com.ricas.lemsai.resouces.OpenAI
+import br.com.ricas.lemsai.resouces.OpenAIResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class OpenAIServiceImpl(
+
+) : OpenAIService{
+
+
+    override fun exec(question: String): OpenAIResponse {
+
+        val openAI = OpenAI()
+        openAI.makeQuestion(question)
+    }
+
+}
