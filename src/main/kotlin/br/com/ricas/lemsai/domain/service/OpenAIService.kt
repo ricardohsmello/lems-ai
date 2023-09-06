ea4f597832c69494aa35b05e9383b92c8fcada48
Ricardo <ricardohsmello@gmail.com>
2023-09-06 13:48:48 -0300

    refactor service

--- src/main/kotlin/br/com/ricas/lemsai/domain/service/OpenAIService.kt
+++ src/main/kotlin/br/com/ricas/lemsai/domain/service/OpenAIService.kt
@@ -1,8 +1,5 @@
 package br.com.ricas.lemsai.domain.service
 
-import br.com.ricas.lemsai.domain.ai.OpenAIResponse
-import java.lang.StringBuilder
-
 interface OpenAIService {
-    fun requestChatGPT(input: String): StringBuilder?
+    fun createArticle(input: String): StringBuilder
 }
