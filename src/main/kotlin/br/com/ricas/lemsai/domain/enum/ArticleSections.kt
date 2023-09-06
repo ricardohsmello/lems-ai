b6116e629efc3dca9f84382de17ed94a6e35c0c5
Ricardo <ricardohsmello@gmail.com>
2023-09-06 13:08:13 -0300

    created ArticleSections

---
+++ src/main/kotlin/br/com/ricas/lemsai/domain/enum/ArticleSections.kt
@@ -0,0 +1,9 @@
+package br.com.ricas.lemsai.domain.enum
+
+enum class ArticleSections(val description: String) {
+    INTRODUCTION("Introdução"),
+    SECTION_1("Seção 1"),
+    SECTION_2("Seção 2"),
+    SECTION_3("Seção 3"),
+    CONCLUSION("Conclusão")
+}
