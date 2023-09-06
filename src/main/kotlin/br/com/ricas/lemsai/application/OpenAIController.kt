b8ab62d9aebe5eac1f802b4ff9f2210bfeb482d1
Ricardo <ricardohsmello@gmail.com>
2023-09-06 13:49:38 -0300

    refactor controller

--- src/main/kotlin/br/com/ricas/lemsai/application/OpenAIController.kt
+++ src/main/kotlin/br/com/ricas/lemsai/application/OpenAIController.kt
@@ -1,18 +1,29 @@
 package br.com.ricas.lemsai.application
 
 import br.com.ricas.lemsai.domain.service.OpenAIService
+import br.com.ricas.lemsai.domain.util.TimeExecutionControl
 import org.springframework.web.bind.annotation.*
 import java.lang.StringBuilder
-
 @RestController
 @RequestMapping("openai")
 class OpenAIController(
-    val openAIService: OpenAIService
+    val openAIService: OpenAIService,
+    val timeExecutionControl: TimeExecutionControl
 ) {
+    lateinit var openAiResponse: StringBuilder
     @PostMapping("/request")
     fun request(@RequestParam theme: String): StringBuilder? {
-        val openAiResponse = openAIService.requestChatGPT(theme)
-        println(openAiResponse)
+       println("Starting article creation ...")
+
+        val elapsedTime = timeExecutionControl.start {
+
+            openAiResponse = openAIService.createArticle(theme)
+            println(openAiResponse)
+        }
+
+        val formattedTime = timeExecutionControl.formatElapsedTime(elapsedTime)
+
+        println("\n\nTotal time for generation: $formattedTime")
 
         return openAiResponse
     }
