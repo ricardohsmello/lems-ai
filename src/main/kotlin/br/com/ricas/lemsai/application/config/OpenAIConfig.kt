0774b259007f0fe3e562ab87758bf725098b2b6e
Ricardo <ricardohsmello@gmail.com>
2023-09-07 20:36:49 -0300

    created config

---
+++ src/main/kotlin/br/com/ricas/lemsai/application/config/OpenAIConfig.kt
@@ -0,0 +1,55 @@
+package br.com.ricas.lemsai.application.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenAIConfig {
+    @Value("\${openai.api.key}")
+    private lateinit var apiKey: String
+
+    @Value("\${openai.api.model}")
+    private lateinit var apiModel: String
+
+    @Value("\${openai.api.url}")
+    private lateinit var apiURL: String
+
+    @Value("\${openai.api.message.base}")
+    private val baseMessage: String = ""
+
+    @Value("\${openai.api.message.next}")
+    private val nextMessage: String = ""
+
+    @Value("\${openai.api.message.detail}")
+    private val messageDetail: String = ""
+    @Bean
+    fun apiKey(): String {
+        return apiKey
+    }
+
+    @Bean
+    fun apiModel(): String {
+        return apiModel
+    }
+
+    @Bean
+    fun apiURL(): String {
+        return apiURL
+    }
+
+    @Bean
+    fun baseMessage(): String {
+        return baseMessage
+    }
+
+    @Bean
+    fun nextMessage(): String {
+        return nextMessage
+    }
+
+    @Bean
+    fun messageDetail(): String {
+        return messageDetail
+    }
+}
