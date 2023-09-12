8bdb49d50f7fe7fe86b2c97db734747a89b4f66d
Ricardo <ricardohsmello@gmail.com>
2023-09-11 23:59:36 -0300

    refactor controller

---
+++ src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/CreateArticleUseCaseImpl.kt
@@ -0,0 +1,87 @@
+package br.com.ricas.lemsai.domain.usecase.impl
+
+import br.com.ricas.lemsai.domain.ai.AIMessage
+import br.com.ricas.lemsai.domain.usecase.CreateArticleUseCase
+import br.com.ricas.lemsai.domain.usecase.PrepareArticleContentUseCase
+import br.com.ricas.lemsai.domain.usecase.CreateArticleSectionsTitleUseCase
+import br.com.ricas.lemsai.domain.usecase.PrepareArticleStructureUseCase
+import br.com.ricas.lemsai.domain.util.TimeExecutionControl
+import org.springframework.stereotype.Service
+
+@Service
+class CreateArticleUseCaseImpl(
+    val timeExecutionControl: TimeExecutionControl,
+    val createArticleSectionsTitleUseCase: CreateArticleSectionsTitleUseCase,
+    val prepareArticleStructureUseCase: PrepareArticleStructureUseCase,
+    val prepareArticleContentUseCase: PrepareArticleContentUseCase
+) : CreateArticleUseCase {
+
+    lateinit var completeArticle: StringBuilder
+
+    override fun exec(
+        themeTitle: String,
+        sectionsNumber: String,
+        subSectionNumber: String
+    ): StringBuilder {
+
+        println("Starting article creation ...")
+
+
+
+
+//        val elapsedTime = timeExecutionControl.start {
+//            completeArticle = prepareArticleContentUseCase.exec(
+//                getArticleStructureBase(
+//                    prepareArticleStructureUseCase.exec(
+//                        themeTitle,
+//                        sectionsNumber,
+//                        subSectionNumber
+//                    )
+//                ),
+//                totalSections = Integer.parseInt(sectionsNumber) + 2
+//            )
+//        }
+//
+//        val formattedTime = timeExecutionControl.formatElapsedTime(elapsedTime)
+//
+//        println("\n\nTotal time for generation: $formattedTime")
+
+        return completeArticle
+
+    }
+
+    private fun getTitleAndContent(
+        themeTitle: String,
+        index: Int,
+        sectionsNumber: Int,
+        context: StringBuilder
+
+    ) : Map<String, String> {
+
+        createArticleSectionsTitleUseCase.exec(themeTitle, getSectionTitle(index, sectionsNumber), context) // Introducao: Corinthians, uma historia linda
+//
+//        val mapOf = mapOf(
+//             to prepareArticleSectionsTitleUseCase.exec(themeTitle, getSectionTitle(sectionsNumber))
+//            1990 to "Julio fernandes"
+//        )
+
+
+    }
+
+    private fun getSectionTitle(index: Int, sectionsNumber: Int): String {
+       return when (index) {
+           0 -> "Introdução"
+           sectionsNumber -> "Conclusão"
+           else -> "Seção " + (index + 1)
+       }
+
+    }
+
+    private fun getArticleStructureBase(articleStructure: StringBuilder) =
+         listOf(
+            AIMessage(
+                role = "user",
+                content = articleStructure.toString()
+            )
+        )
+}
