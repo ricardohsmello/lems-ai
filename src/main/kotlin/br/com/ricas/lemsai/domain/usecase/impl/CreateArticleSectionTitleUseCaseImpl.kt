41105a3c043e91cb25f4e20fdcdca29ffb754571
Ricardo <ricardohsmello@gmail.com>
2023-09-13 01:35:09 -0300

    Refactoring

--- src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/CreateArticleSectionTitleUseCaseImpl.kt
+++ src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/CreateArticleSectionTitleUseCaseImpl.kt
@@ -3,33 +3,27 @@ package br.com.ricas.lemsai.domain.usecase.impl
 import br.com.ricas.lemsai.application.config.OpenAIConfig
 import br.com.ricas.lemsai.domain.ai.AIMessage
 import br.com.ricas.lemsai.domain.port.OpenAIRequestPort
-import br.com.ricas.lemsai.domain.usecase.PrepareArticleStructureUseCase
+import br.com.ricas.lemsai.domain.usecase.CreateArticleSectionTitleUseCase
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class PrepareArticleStructureUseCaseImpl(
+class CreateArticleSectionTitleUseCaseImpl(
     @Autowired private val openAIConfig: OpenAIConfig,
     private val openAIRequestPort: OpenAIRequestPort
-) : PrepareArticleStructureUseCase {
-    override fun exec(
-        themeTitle: String,
-        sectionsNumber: String,
-        subSectionNumber: String
-    ): StringBuilder {
-
-        val articleStructureContent = openAIConfig.baseMessage().replace(
-            "{themeTitle}", themeTitle
+) : CreateArticleSectionTitleUseCase {
+    override fun exec(sectionTitle: String, articleTheme: String): String {
+        val message = openAIConfig.articleSectionTitle().replace(
+            "{articleSectionTitle}", sectionTitle
         ).replace(
-            "{sectionNumber}", sectionsNumber
-        ).replace(
-            "{subSectionNumber}", subSectionNumber
+            "{articleMainTheme}", articleTheme
         )
+
 
         val messages = listOf(
             AIMessage(
                 role = "user",
-                content = articleStructureContent
+                content = message
             )
         )
 
@@ -37,8 +31,9 @@ class PrepareArticleStructureUseCaseImpl(
             openAIConfig.apiModel(),
             openAIConfig.apiKey(),
             openAIConfig.apiURL(),
-            messages)
+            messages
+        )
 
-        return StringBuilder(requestChatGPT.choices[0].message.content)
+        return requestChatGPT.choices[0].message.content
     }
 }
