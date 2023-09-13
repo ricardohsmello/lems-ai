a244fe26a1c51c53f4cec58dc186ec0d37396747
Ricardo <ricardohsmello@gmail.com>
2023-09-13 10:07:29 -0300

    Refactoring

---
+++ src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/PrepareArticleConclusionUseCaseImpl.kt
@@ -0,0 +1,37 @@
+package br.com.ricas.lemsai.domain.usecase.impl
+
+import br.com.ricas.lemsai.application.config.OpenAIConfig
+import br.com.ricas.lemsai.domain.entity.Section
+import br.com.ricas.lemsai.domain.usecase.CreateSectionUseCase
+import br.com.ricas.lemsai.domain.usecase.OpenAIRequestUseCase
+import br.com.ricas.lemsai.domain.usecase.PrepareArticleConclusionUseCase
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class PrepareArticleConclusionUseCaseImpl(
+    @Autowired private val openAIConfig: OpenAIConfig,
+    private val openAIRequestUseCase: OpenAIRequestUseCase
+
+) : PrepareArticleConclusionUseCase {
+    override fun exec(
+        title: String,
+        context: StringBuilder
+    ): Section {
+        println("Starting Conclusion creation for theme $title")
+
+        openAIConfig.articleConclusionContent()
+            .replace(
+                "{articleMainTheme}", title
+            ).replace(
+                "{context}", context.toString()
+            ).also { titleMessage ->
+
+                return Section(
+                    title = StringBuilder("Conclusão"),
+                    isSubSection = false,
+                    content = openAIRequestUseCase.requestAnswer(titleMessage)
+                )
+            }
+    }
+}
