40cf52b069b046299a75fe4d49a429296edc05d9
Ricardo <ricardohsmello@gmail.com>
2023-09-12 23:56:46 -0300

    Article class

--- src/main/kotlin/br/com/ricas/lemsai/domain/entity/Article.kt
+++ src/main/kotlin/br/com/ricas/lemsai/domain/entity/Article.kt
@@ -2,17 +2,11 @@ package br.com.ricas.lemsai.domain.entity
 
 data class Article(
     val title: String,
-    val sections: List<Section>
-) {
-}
-
-data class Section(
-    val numeration: Int,
-    val title: String,
-    val subSection: List<SubSection>
+    val sections: List<Section>?
 )
-
-data class SubSection(
-    val numeration: Int,
-    val title: String
+data class Section(
+    val order: Int,
+    val title: String,
+    val isSubSection: Boolean,
+    val content: StringBuilder
 )
