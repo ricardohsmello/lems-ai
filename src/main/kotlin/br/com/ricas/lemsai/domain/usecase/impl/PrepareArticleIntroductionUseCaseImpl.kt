2111a76afaa42554f1b2620c925633e26b5fdba8
Ricardo <ricardohsmello@gmail.com>
2023-09-13 10:07:27 -0300

    Refactoring

---
+++ src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/PrepareArticleIntroductionUseCaseImpl.kt
@@ -0,0 +1,37 @@
+package br.com.ricas.lemsai.domain.usecase.impl
+
+import br.com.ricas.lemsai.application.config.OpenAIConfig
+import br.com.ricas.lemsai.domain.entity.Section
+import br.com.ricas.lemsai.domain.usecase.PrepareArticleIntroductionUseCase
+import br.com.ricas.lemsai.domain.usecase.CreateSectionUseCase
+import br.com.ricas.lemsai.domain.usecase.OpenAIRequestUseCase
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class PrepareArticleIntroductionUseCaseImpl(
+    @Autowired private val openAIConfig: OpenAIConfig,
+    private val createSectionUseCase: CreateSectionUseCase,
+    private val openAIRequestUseCase: OpenAIRequestUseCase
+) : PrepareArticleIntroductionUseCase {
+    override fun exec(
+        title: String
+    ): Section {
+
+        println("Starting introduction creation for theme $title")
+
+        openAIConfig.articleIntroductionTitle()
+            .replace(
+                "{articleMainTheme}", title
+            ).also { titleMessage ->
+                return createSectionUseCase.exec(
+                    titlePropertyMessage = titleMessage,
+                    contentPropertyMessage = openAIConfig.articleSectionContent()
+                        .replace(
+                            "{content}", openAIRequestUseCase.requestAnswer(titleMessage).toString()
+                        ),
+                    isSubSection = false
+                )
+            }
+    }
+}
