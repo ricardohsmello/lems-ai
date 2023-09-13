a2dbc336dc0bd1ceb279c3ad668b5f4ff4710e53
Ricardo <ricardohsmello@gmail.com>
2023-09-13 10:07:39 -0300

    Refactoring

---
+++ src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/OpenAIRequestUseCaseImpl.kt
@@ -0,0 +1,32 @@
+package br.com.ricas.lemsai.domain.usecase.impl
+
+import br.com.ricas.lemsai.application.config.OpenAIConfig
+import br.com.ricas.lemsai.domain.ai.AIMessage
+import br.com.ricas.lemsai.domain.port.OpenAIRequestPort
+import br.com.ricas.lemsai.domain.usecase.OpenAIRequestUseCase
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class OpenAIRequestUseCaseImpl(
+    @Autowired private val openAIConfig: OpenAIConfig,
+    private val openAIRequestPort: OpenAIRequestPort
+) : OpenAIRequestUseCase{
+    override fun requestAnswer(content: String): StringBuilder {
+        val messages = listOf(
+            AIMessage(
+                role = "user",
+                content = content
+            )
+        )
+
+        val requestChatGPT = openAIRequestPort.requestChatGPT(
+            openAIConfig.apiModel(),
+            openAIConfig.apiKey(),
+            openAIConfig.apiURL(),
+            messages
+        )
+
+        return StringBuilder(requestChatGPT.choices[0].message.content)
+    }
+}
