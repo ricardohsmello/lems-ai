03eb1f37026f6e3b8be393d30b7bb71694c11a91
Ricardo <ricardohsmello@gmail.com>
2023-09-13 10:07:37 -0300

    Refactoring

---
+++ src/main/kotlin/br/com/ricas/lemsai/domain/usecase/PrepareArticleConclusionUseCase.kt
@@ -0,0 +1,11 @@
+package br.com.ricas.lemsai.domain.usecase
+
+import br.com.ricas.lemsai.domain.entity.Section
+import java.lang.StringBuilder
+
+interface PrepareArticleConclusionUseCase {
+    fun exec(
+        title: String,
+        context: StringBuilder
+    ): Section
+}
