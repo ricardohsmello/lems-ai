47e9879aea35afa83115b5d6df283693c06428df
Ricardo <ricardohsmello@gmail.com>
2023-09-14 23:50:54 -0300

    Refactoring

---
+++ src/test/kotlin/br/com/ricas/lemsai/fakedata/fakeOpenAIResponse.kt
@@ -0,0 +1,28 @@
+package br.com.ricas.lemsai.fakedata
+
+import br.com.ricas.lemsai.domain.ai.Choice
+import br.com.ricas.lemsai.domain.ai.Message
+import br.com.ricas.lemsai.domain.ai.OpenAIResponse
+import br.com.ricas.lemsai.domain.ai.Usage
+
+val fakeOpenAIResponse = OpenAIResponse(
+    id = "fake-id",
+    `object` = "fake-object",
+    created = System.currentTimeMillis(),
+    model = "fake-model",
+    choices = listOf(
+        Choice(
+            index = 0,
+            message = Message(
+                role = "assistant",
+                content = "Fake assistant response"
+            ),
+            finish_reason = "fake-finish-reason"
+        )
+    ),
+    usage = Usage(
+        prompt_tokens = 10,
+        completion_tokens = 20,
+        total_tokens = 30
+    )
+)
