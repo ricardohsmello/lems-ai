607241cdb0806b196637ddaf9a7df6647e7aea6f
Ricardo <ricardohsmello@gmail.com>
2023-09-15 13:51:12 -0300

    Refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.3"
 	id("io.spring.dependency-management") version "1.1.3"
-
 	id ("org.sonarqube") version "3.0"
 	id("jacoco")
-
-
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
+
 }
 
 group = "br.com.ricas"
@@ -23,14 +21,13 @@ repositories {
 	mavenCentral()
 }
 
-
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.mockk:mockk:1.12.0")
+
 }
 
 tasks.withType<KotlinCompile> {
@@ -40,8 +37,6 @@ tasks.withType<KotlinCompile> {
 	}
 }
 
-
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
