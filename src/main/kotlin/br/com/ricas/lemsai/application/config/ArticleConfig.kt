09b1b0eb54d42d9f8ee804ef35e1365f7e0ab2d6
Ricardo <ricardohsmello@gmail.com>
2023-09-15 01:45:11 -0300

    Refactoring

---
+++ src/main/kotlin/br/com/ricas/lemsai/application/config/ArticleConfig.kt
@@ -0,0 +1,19 @@
+package br.com.ricas.lemsai.application.config
+
+object ArticleConfig {
+    private var minCharSection: Int = 100
+    private var maxCharSection: Int = 200
+
+    fun setCharSectionValues(min: Int, max: Int) {
+        minCharSection = min
+        maxCharSection = max
+    }
+
+    fun getMinCharSection(): Int {
+        return minCharSection
+    }
+
+    fun getMaxCharSection(): Int {
+        return maxCharSection
+    }
+}
