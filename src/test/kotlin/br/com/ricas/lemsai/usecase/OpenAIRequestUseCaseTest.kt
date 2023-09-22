6b07ec00d16e261064e18727c19c8453078027f3
Ricardo <ricardohsmello@gmail.com>
2023-09-21 21:47:41 -0300

    Refactoring

--- src/test/kotlin/br/com/ricas/lemsai/usecase/OpenAIRequestUseCaseTest.kt
+++ src/test/kotlin/br/com/ricas/lemsai/usecase/OpenAIRequestUseCaseTest.kt
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-
 @SpringBootTest(classes = [LemsAiApplication::class])
 class OpenAIRequestUseCaseImplTest {
 
