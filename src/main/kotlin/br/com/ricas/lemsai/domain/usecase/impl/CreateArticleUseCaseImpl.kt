78cd081597994266679f7fccd987793b3a26191b
Ricardo <ricardohsmello@gmail.com>
2023-09-21 21:48:43 -0300

    Refactoring

--- src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/CreateArticleUseCaseImpl.kt
+++ src/main/kotlin/br/com/ricas/lemsai/domain/usecase/impl/CreateArticleUseCaseImpl.kt
@@ -9,7 +9,6 @@ import br.com.ricas.lemsai.domain.usecase.PrepareArticleSectionUseCase
 import br.com.ricas.lemsai.domain.util.TimeExecutionControl
 import br.com.ricas.lemsai.domain.util.logger
 import org.springframework.stereotype.Service
-
 @Service
 class CreateArticleUseCaseImpl(
     private val timeExecutionControl: TimeExecutionControl,
